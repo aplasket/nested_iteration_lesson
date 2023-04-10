80a6a393945ab2ae14ef144bbec7856a22f021c0
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-10 11:28:55 -0400

    add long animals using select

--- warmup/warmup1.rb
+++ warmup/warmup1.rb
@@ -10,6 +10,13 @@ p new_string_arr
 
 
 # 2. Return an array of animals with a length >= 4.
+  # wouldn't want to use map here because this will return an array of booleans of the same number
+  # of elements of original array and just replaces the block with the statement we're asking it to 
+  
+# great_size_animals = animals.map {|animal| animal.length >= 4}
+# p great_size_animals
+#=> [false, false, true, true, true, true]
 
-
+great_size_animals = animals.select {|animal| animal.length >= 4}
+p great_size_animals
 
