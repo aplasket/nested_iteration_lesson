d1aaffc1dde5635f52571efc57a2ecc0edc36cf1
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-10 11:22:27 -0400

    add first exercise

--- warmup/warmup1.rb
+++ warmup/warmup1.rb
@@ -5,7 +5,8 @@ animals = [:dog, :cat, :zebra, :quokka, :unicorn, :bear]
 # Use an enumerable to
 # 1. Return an array of animals as strings.
 
-
+new_string_arr = animals.map {|animal| animal.to_s}
+p new_string_arr
 
 
 # 2. Return an array of animals with a length >= 4.
